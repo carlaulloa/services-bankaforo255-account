7aba7f59de7e2ba7d90bf28e11a4efc2a41fbeb8
carlaulloa <carla.ulloa.lp@gmail.com>
2020-04-11 07:47:45 -0500

    feat:base

---
+++ src/main/java/com/app/aforo255/account/ServucesBankaforo255AccountApplication.java
@@ -0,0 +1,13 @@
+package com.app.aforo255.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServucesBankaforo255AccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServucesBankaforo255AccountApplication.class, args);
+	}
+
+}
